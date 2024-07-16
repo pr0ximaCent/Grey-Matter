5ff80b69c56c841a4b4c34f9d813cb50d9d71c10
Abu Md. Masbah Uddin <masbahuddin60@gmail.com>
2024-07-16 20:02:38 +0600

    added wannabe_a_researcher top 6 bd

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import JRank from './JRank.jsx';
+import WannaBeResearcher from "./components/design/wanna_be_researcher.jsx";
 
 const router = createBrowserRouter([
   {
@@ -14,7 +15,10 @@ const router = createBrowserRouter([
   {
     path: '/journal-ranking',
     element: <JRank />,
-  },
+  },    {
+    path: "/wannabe-a-researcher",
+    element: <WannaBeResearcher />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(

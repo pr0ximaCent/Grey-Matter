fce19f2d3d1fa91b156341e9d53f1201252f5905
proximaCent <sabikaftahee01@gmail.com>
2024-07-16 15:49:50 +0600

    header glow icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,8 @@ const Header = () => {
       if (element) {
         element.scrollIntoView({ behavior: "smooth" });
       }
-    } else if (url === "Home") { //home link alada handle kora hoise, see if you can make it better without altering functionality
+    } else if (url === "Home") {
+      //home link alada handle kora hoise, see if you can make it better without altering functionality
       navigate("/");
     } else {
       navigate("/");
@@ -58,10 +59,27 @@ const Header = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-7/90 lg:backdrop-blur-sm bg-n-8/90 backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="/">
+        <a
+          className="block w-[12rem] xl:mr-8"
+          href="/"
+          style={{ filter: "drop-shadow(0 0 5px rgba(255, 255, 255, 0.5))" }}
+        >
           <div className="flex items-center">
-            <img src={greymatter} width={60} height={40} alt="Grey Matter" />
-            <span className="ml-1 text-n-1">Grey Matter</span>
+            <img
+              src={greymatter}
+              width={60}
+              height={40}
+              alt="Grey Matter"
+              style={{
+                filter: "drop-shadow(0 0 5px rgba(255, 255, 255, 0.09))",
+              }}
+            />
+            <span
+              className="ml-1 text-n-1"
+              style={{ textShadow: "0 0 2px rgba(255, 255, 255, 0.5)" }}
+            >
+              Grey Matter
+            </span>
           </div>
         </a>
 
@@ -77,7 +95,7 @@ const Header = () => {
                 }
                 onClick={(event) => handleClick(event, elem.url)}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-sm lg:font-semibold ${
-                  elem.url === location.hash ? 'text-n-1' : 'text-n-1/50'
+                  elem.url === location.hash ? "text-n-1" : "text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {elem.title}
@@ -87,28 +105,26 @@ const Header = () => {
           <HamburgerMenu />
         </nav>
 
-          <div className="flex space-x-3 items-center">
+        <div className="flex space-x-3 items-center">
+          <a
+            href="#signin"
+            className="text-gray-300 hover:text-white transition-colors duration-300 py-2 px-12 text-xs font-medium uppercase"
+            onClick={handleLoginOpen}
+          >
+            Sign In
+          </a>
 
-            <a
-              href="#signin"
-              className="text-gray-300 hover:text-white transition-colors duration-300 py-2 px-12 text-xs font-medium uppercase"
-              onClick={handleLoginOpen}
-            >
-              Sign In
-            </a>
-
-            <Button
-              href="#login"
-              className="text-xs font-medium h-6 w-12 transition-all duration-300 hover:bg-gradient-to-r hover:text-black"
-              onClick={handleSignupOpen}
-            >
-              Join!
-            </Button>
-          </div>
-
-          <HamburgerMenu />
+          <Button
+            href="#login"
+            className="text-xs font-medium h-6 w-12 transition-all duration-300 hover:bg-gradient-to-r hover:text-black"
+            onClick={handleSignupOpen}
+          >
+            Join!
+          </Button>
         </div>
-      
+
+        <HamburgerMenu />
+      </div>
 
       <SignUpModal open={signupOpen} onClose={handleSignupClose} />
       <SignInModal open={loginOpen} onClose={handleLoginClose} />

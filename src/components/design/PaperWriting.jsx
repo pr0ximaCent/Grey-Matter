0ee80226eddd43d76597926b38679a4f69dcfbc8
proximaCent <sabikaftahee01@gmail.com>
2024-08-29 00:14:25 +0600

    try installing npm install react-carousel

--- src/components/design/PaperWriting.jsx
+++ src/components/design/PaperWriting.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Grid, Typography } from "@mui/material";
 import "tailwindcss/tailwind.css";
 import Header from "../Header.jsx";
